b2a8b05b80c98618b90b834ec7e83cafd0abb96d
Edih Goodluck <77158925+Codablack23@users.noreply.github.com>
2024-08-08 10:40:42 +0100

    Update db.ts

--- config/db.ts
+++ config/db.ts
@@ -6,13 +6,14 @@ const dbConfig = {
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     db:process.env.DB_NAME,
-    environment:process.env.ENV
+    host:process.env.DB_HOST,
+    environment:process.env.ENV,
 }
 
 console.log(dbConfig)
 const db:Options = {
     dialect:"mysql",
-    host:"52.49.171.158"
+    host:dbConfig.host || "52.49.171.158"
 };
 
 export const sequelize = new Sequelize(dbConfig.db as string,
